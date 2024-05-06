f801c44c3c24582c112b7e35b77f59f44be1d26d
conejobasketsuma99 <164917697+conejobasketsuma99@users.noreply.github.com>
2024-05-06 17:35:57 -0300

    lool

---
+++ ejercicio1.py
@@ -0,0 +1,12 @@
+#Crear una lista llamada meses y que almacene el nombre de los doce meses del año.
+#Mostrar por pantalla los doce nombres utilizando la funcion print().
+
+meses = ["Enero","Febrero","Marzo","Abril", "Mayo","Junio","Julio","Agosto","Septiembre","Noviembre","Diciembre"];
+n=5
+for i in range(len(meses)):
+    print(meses[i])
+
+if n == 5:
+    print(n)
+else:
+    print(n)
---
+++ ejercicio2.py
@@ -0,0 +1,19 @@
+def ejercicio2():
+    valores = [True, 5, False, "hola", "adios", 2]
+
+    if len(valores[3]) > len(valores[4]):
+        print("hola es mayor que adios")
+        print(valores[0])
+
+    else:
+        print("adios es mayor que hola")
+        print(valores[2])
+
+    print(valores[1] + valores[5])
+    print(valores[1] - valores[5])
+    print(valores[1] * valores[5])
+    print(valores[1] / valores[5])
+    print(valores[5] / valores[1])
+    print(valores[5] - valores[1])
+
+ejercicio2()
