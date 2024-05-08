e6f4c82ae2061a01d6bfab04c0f510f5b1ef412f
conejobasketsuma99 <164917697+conejobasketsuma99@users.noreply.github.com>
2024-05-08 15:20:22 -0300

    v2
    ola

---
+++ E1.py
@@ -0,0 +1,25 @@
+def e1():
+    # Leer los dos números enteros
+    numero1 = int(input("Ingrese el primer número entero: "))
+    numero2 = int(input("Ingrese el segundo número entero: "))
+
+    # Calcular la suma
+    suma = numero1 + numero2
+
+    # Informar el resultado
+    print("La suma de", numero1, "y", numero2, "es:", suma)
+e1()
+
+def e2():
+    # Leer los dos números enteros
+    numero1 = int(input("Ingrese el primer número entero: "))
+    numero2 = int(input("Ingrese el segundo número entero: "))
+    numero3 = int(input("Ingrese el tercer número entero: "))
+    numero4 = int(input("Ingrese el cuarto número entero: "))
+
+    # Calcular la suma
+    suma = numero1 + numero2 +  numero3 + numero4
+
+    # Informar el resultado
+    print("La suma de", numero1, numero2, numero3, "y", numero4, "es:", suma)
+e2()
---
+++ E3.py
@@ -0,0 +1,11 @@
+def e3():
+    # Leer los dos lados del rectángulo
+    lado1 = int(input("Ingrese el primer lado del rectángulo: "))
+    lado2 = int(input("Ingrese el segundo lado del rectángulo: "))
+
+    # Calcular la superficie del rectángulo
+    superficie = lado1 * lado2
+
+    # Informar la superficie del rectángulo
+    print("La superficie del rectángulo es:", superficie)
+e3()
